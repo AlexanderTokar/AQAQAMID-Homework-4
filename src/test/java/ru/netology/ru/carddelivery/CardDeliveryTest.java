da8f43a108ad92732ef0d7da77bbbb41cb65ccfc
AlexanderTokar <alextokar@inbox.ru>
2022-09-11 11:39:07 +0300

    Made mistake at CardDeliveryTest.java to fail test

---
+++ src/test/java/ru/netology/ru/carddelivery/CardDeliveryTest.java
@@ -0,0 +1,45 @@
+package ru.netology.ru.carddelivery;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+
+    String deliveryDay (int days) {
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @BeforeEach
+    void WebOpen() {
+        Configuration.holdBrowserOpen=true;
+        open("http://localhost:9999");
+    }
+
+
+    @Test
+    void shouldSubmitRequestHappyPath() {
+        String deliveryDate = deliveryDay(3);
+        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id=date] input").setValue(deliveryDate);
+        $("[data-test-id='name'] input").setValue("Джон Голт1");
+        $("[data-test-id='phone'] input").setValue("+79109109191");
+        $("[data-test-id='agreement']").click();
+        $x("//*[contains(text(),'Забронировать')]").click();
+        $x("//*[@data-test-id='notification']").shouldHave(Condition.text("Успешно! Встреча успешно забронирована на " + deliveryDate), Duration.ofSeconds(15));
+    }
+
+
+}
